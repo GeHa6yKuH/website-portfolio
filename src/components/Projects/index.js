1797fe8a0a5ea7244fb3d6dcda19af317d8d6691
yellnessity <diaz99@mail.ru>
2024-05-21 19:12:44 +0200

    test commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,23 +1,16 @@
-import './index.scss';
-
+import './index.scss'
 
 const Projects = () => {
-
-
-    return (
-        <>
-        <div className='container'>
-            <div className='project'>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book.
-                </p>
-                <img src='https://picsum.photos/id/11/500/300' />
-            </div>
+  return (
+    <>
+      <div className="container">
+        <div className="project">
+          <p>TEST</p>
+          <img src="https://picsum.photos/id/11/500/300" />
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default Projects
