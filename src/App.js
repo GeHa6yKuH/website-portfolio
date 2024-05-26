741d29e57aa0ae1db894ca04b4e42d3045ded27c
Bogdans <bigdan110702@gmail.com>
2024-05-26 19:03:33 +0300

    bug fixes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
@@ -20,65 +20,66 @@ function App() {
 
 
   return (
-    <div
-      style={{ position: 'relative', height: '100vh', overflow: 'auto' }}
-      className="maindiv"
-    >
-      <video autoPlay loop muted className="bg-video">
-        <source src="spacevid.mp4" type="video/mp4" />
-      </video>
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="projects" element={<Projects />} />
-        </Route>
-      </Routes>
-      
       <div
-        style={{
-          position: 'fixed',
-          right: 50,
-          bottom: 0,
-          height: '19.5%',
-          width: '1.5px',
-          backgroundColor: 'yellow',
-        }}
-      ></div>
+        style={{ position: 'relative', height: '100vh', overflow: 'auto' }}
+        className="maindiv"
+      >
 
-      <div className="vertical-text">
-        <AnimatedText
-          letterClass={letterClass}
-          strArray={letterTextArray}
-          idx={0}
-        />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="projects" element={<Projects />} />
+          </Route>
+        </Routes>
+
+        <video autoPlay loop muted className="bg-video">
+          <source src="spacevid.mp4" type="video/mp4" />
+        </video>
+        
+        <div
+          style={{
+            position: 'fixed',
+            right: 50,
+            bottom: 0,
+            height: '19.5%',
+            width: '1.5px',
+            backgroundColor: 'yellow',
+          }}
+        ></div>
+
+        <div className="vertical-text">
+          <AnimatedText
+            letterClass={letterClass}
+            strArray={letterTextArray}
+            idx={0}
+          />
+        </div>
+
+        <ul>
+          <li>
+            <a
+              className="linkedIn"
+              target="_blank"
+              rel="noreffer"
+              href="https://www.linkedin.com/in/bogdans-podaļevičs-7a380529a"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#9f7a7a" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreffer" href="https://github.com/GeHa6yKuH">
+              <FontAwesomeIcon icon={faGithub} color="#9f7a7a" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreffer" href="discord:bigdarian">
+              <FontAwesomeIcon icon={faDiscord} color="#9f7a7a" />
+            </a>
+          </li>
+        </ul>
       </div>
-
-      <ul>
-        <li>
-          <a
-            className="linkedIn"
-            target="_blank"
-            rel="noreffer"
-            href="https://www.linkedin.com/in/bogdans-podaļevičs-7a380529a"
-          >
-            <FontAwesomeIcon icon={faLinkedin} color="#9f7a7a" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreffer" href="https://github.com/GeHa6yKuH">
-            <FontAwesomeIcon icon={faGithub} color="#9f7a7a" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreffer" href="discord:bigdarian">
-            <FontAwesomeIcon icon={faDiscord} color="#9f7a7a" />
-          </a>
-        </li>
-      </ul>
-    </div>
   )
 }
 
